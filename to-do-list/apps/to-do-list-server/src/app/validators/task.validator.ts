9a4708f7462043a558cab82082698754bd5d578f
RoksolianaMatseliukh <rmatseliukh@cloverdynamics.com>
2022-10-03 15:21:13 +0300

    added regex

--- to-do-list/apps/to-do-list-server/src/app/validators/task.validator.ts
+++ to-do-list/apps/to-do-list-server/src/app/validators/task.validator.ts
@@ -1,6 +1,6 @@
 import * as Joi from 'joi';
 
 export const taskValidator = Joi.object({
-  name: Joi.string().trim().required(),
+  name: Joi.string().replace(/\s+/g, ' ').trim().required(),
   listId: Joi.number().min(1).required()
 });
